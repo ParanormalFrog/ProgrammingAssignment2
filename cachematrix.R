64a27e055d5ebc32268b53ef6dcd61c10b022e4e
ParanormalFrog <Johannes.schuldt.jensen@gmail.com>
2016-02-21 16:24:21 +0100

    Assignment2
    My solution to the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+#These functions allows storage of inverse matrices, instead of having to calculate them each time 
 
-## Write a short comment describing this function
+
+#this functions converts a matrix to a list of functions, that allow retrieval and edit of the input matrix by using get and set, as well the inverse matrix by the two remaining functions.
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        im <- NULL
+        set <- function(y) {
+                x <<- y
+                im <<- NULL
+        }
+        get <- function() x
+        setmatrix <- function(matrix) im <<- matrix
+        getmatrix <- function() im
+        list(set = set, get = get,
+             setmatrix = setmatrix,
+             getmatrix = getmatrix)
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#this function evaluates whether an inverse matrix has been calculated for the input matrix. If it has, that matrix is retrieved. If it hasn't. That matrix is calculated and cached. It takes as argument the list creatded from the previous function
+cachesolve <- function(x, ...) {
+        im <- x$getmatrix()
+        if(!is.null(im)) {
+                message("getting cached matrix")
+                return(im)
+        }
+        data <- x$get()
+        im <- solve(data)
+        x$setmatrix(im)
+        im
 }
